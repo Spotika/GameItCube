01f4f1d7d96fd7a44c4b168c76819ffdd21ffce0
Spotika <andrey.a.cherkashin@gmail.com>
2023-05-03 22:44:05 +1000

    added main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pygame
 from Interface import *
 from App import *
 from Screen import *
+from MainMenu import *
 
 
 def main():
     Screen.begin()  # init
+    MainMenu.begin()
 
 
 if __name__ == '__main__':
