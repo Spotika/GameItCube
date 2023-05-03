70301d5823a9ba8589f801a7d941e76fa14be879
Spotika <andrey.a.cherkashin@gmail.com>
2023-05-03 22:43:47 +1000

    added App.py

---
+++ App.py
@@ -0,0 +1,13 @@
+class App:
+    running: bool = True
+
+    def __new__(cls, *args, **kwargs):
+        return None
+
+    @classmethod
+    def begin(cls):
+        ...
+
+    @classmethod
+    def end(cls):
+        ...
