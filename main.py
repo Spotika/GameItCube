831d0666d35533eab33a7b1736c864fa3cea2512
Spotika <andrey.a.cherkashin@gmail.com>
2023-05-03 21:57:48 +1000

    added main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pygame
+from Interface import *
+from App import *
+from Screen import *
+
+
+def main():
+    Screen.begin()  # init
 
 
 if __name__ == '__main__':
-    ...
+    main()
+    exit(0)
