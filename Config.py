91471162f723682cec7b27ff643bd871cd452812
Spotika <andrey.a.cherkashin@gmail.com>
2023-05-04 23:27:08 +1000

    добавлены текстуры платформы и обновлён конфиг

--- Config.py
+++ Config.py
@@ -1,5 +1,14 @@
-BACK_GROUND_SPRITE_LAYER = -50
+
+# files
 BACK_GROUND_IMG_FILE_PATH = "media/img/BackGround.png"
 START_BUTTON_IMG_FILE_PATH = "media/img/StarButton.png"
+PLATFORM_LEFT_CORNER = "media/img/platformLeftCorner.png"
+PLATFORM_RIGHT_CORNER = "media/img/platformRightCorner.png"
+PLATFORM_CENTER = "media/img/platformCenter.png"
+
+# Layers
+BACK_GROUND_SPRITE_LAYER = -50
 BUTTON_SPRITE_LAYER = 50
+
+# misc
 FPS = 60
